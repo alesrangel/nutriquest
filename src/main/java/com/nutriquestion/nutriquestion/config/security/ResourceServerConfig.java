e7e1aa4115d07f0ff33b1ea3d6ef9947e2441295
alesrangel <alesrangel15@gmail.com>
2022-05-14 17:44:53 -0300

    cors

--- src/main/java/com/nutriquestion/nutriquestion/config/security/ResourceServerConfig.java
+++ src/main/java/com/nutriquestion/nutriquestion/config/security/ResourceServerConfig.java
@@ -3,22 +3,21 @@ package com.nutriquestion.nutriquestion.config.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import com.fasterxml.jackson.core.filter.TokenFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -30,54 +29,61 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Autowired 
 	private JwtTokenStore tokenStore;
 	
-	private static final String ROOT_PATTERN = "/**";
-	private static final String CRIAR_CONTA_PATTERN = "/usuario/nutricionista/**";
+	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
+	
+	private static final String[] ADMIN = {"/usuario/nutricionista"};
+			
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+		resources.tokenStore(tokenStore);
+	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.POST, CRIAR_CONTA_PATTERN).permitAll().and().authorizeRequests()
-				.antMatchers(HttpMethod.GET, ROOT_PATTERN).access("#oauth2.hasScope('read')")
-				.antMatchers(HttpMethod.POST, ROOT_PATTERN).access("#oauth2.hasScope('write')")
-				.antMatchers(HttpMethod.PUT, ROOT_PATTERN).access("#oauth2.hasScope('write')")
-				.antMatchers(HttpMethod.DELETE, ROOT_PATTERN).access("#oauth2.hasScope('write')")
-				.antMatchers(HttpMethod.PATCH, ROOT_PATTERN).access("#oauth2.hasScope('write')").and().cors();
+		
+		// H2
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		http.csrf().disable()
+		.authorizeRequests()
+		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.POST, ADMIN).permitAll()
+		.anyRequest().permitAll();
+		
+		http.cors().configurationSource(corsConfigurationSource());
+//		http.cors().and().csrf().disable();
+//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		http.authorizeRequests().anyRequest().permitAll();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC).permitAll();
+	
+//		 http.authorizeRequests()
+//		    .antMatchers(HttpMethod.POST, "/auth").permitAll()
+//		    .anyRequest().authenticated()
+//		    .and().cors()
+//		    .and().csrf().disable()
+//		    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//		    .and().addFilterBefore(new TokenFilter(tokenService,nutricionistaRepository), UsernamePasswordAuthenticationFilter.class);
 	}
-//	
-//	private static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"};
-//	
-//	private static final String[] ADMIN = {"/usuario/nutricionista"};
-//			
-//	@Override
-//	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-//		resources.tokenStore(tokenStore);
-//	}
-//
-//	@Override
-//	public void configure(HttpSecurity http) throws Exception {
-//		
-//		// H2
-//		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
-//			http.headers().frameOptions().disable();
-//		}
-//		http.csrf().disable()
-//		.authorizeRequests()
-//		.antMatchers(PUBLIC).permitAll()
-//		.antMatchers(HttpMethod.POST, ADMIN).permitAll()
-//		.anyRequest().permitAll();
-//
-////		http.cors().and().csrf().disable();
-////		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-////		http.authorizeRequests().anyRequest().permitAll();
-////		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC).permitAll();
-//	
-////		 http.authorizeRequests()
-////		    .antMatchers(HttpMethod.POST, "/auth").permitAll()
-////		    .anyRequest().authenticated()
-////		    .and().cors()
-////		    .and().csrf().disable()
-////		    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-////		    .and().addFilterBefore(new TokenFilter(tokenService,nutricionistaRepository), UsernamePasswordAuthenticationFilter.class);
-//	}
-//	
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+		corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+		corsConfig.setAllowCredentials(true);
+		corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "content-Type"));
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		return source;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
   
 }
