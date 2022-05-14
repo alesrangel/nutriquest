3edc3e42dc2a33f2940b6f9cb04c6236896fd3f8
alesrangel <alesrangel15@gmail.com>
2022-05-14 18:12:53 -0300

    cors

--- src/main/java/com/nutriquestion/nutriquestion/config/AppConfig.java
+++ src/main/java/com/nutriquestion/nutriquestion/config/AppConfig.java
@@ -1,13 +1,8 @@
 package com.nutriquestion.nutriquestion.config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
@@ -17,13 +12,4 @@ public class AppConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
-//	@Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("*"));
-//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//   }
 }
