f3e1428d16194241cf4af8060cdac00dc76831fb
alesrangel <alesrangel15@gmail.com>
2022-05-14 18:07:40 -0300

    cors

--- src/main/java/com/nutriquestion/nutriquestion/config/security/ResourceServerConfig.java
+++ src/main/java/com/nutriquestion/nutriquestion/config/security/ResourceServerConfig.java
@@ -3,21 +3,15 @@ package com.nutriquestion.nutriquestion.config.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -45,16 +39,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.POST, ADMIN).permitAll()
-		.anyRequest().permitAll();
+//		http.authorizeRequests()
+//		.antMatchers(PUBLIC).permitAll()
+//		.antMatchers(HttpMethod.POST, ADMIN).permitAll()
+//		.anyRequest().permitAll();
 		
 //		http.cors().configurationSource(corsConfigurationSource());
-//		http.cors().and().csrf().disable();
-//		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//		http.authorizeRequests().anyRequest().permitAll();
-//		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC).permitAll();
+		http.cors().and().csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC).permitAll();
 	
 //		 http.authorizeRequests()
 //		    .antMatchers(HttpMethod.POST, "/auth").permitAll()
