1c0cc05db361ce205b2a9f8b84bdee6b8c896fa0
alesrangel <alesrangel15@gmail.com>
2022-05-15 19:59:04 -0300

    cors dos inferno

--- src/main/java/com/nutriquestion/nutriquestion/config/CorsConfiguration.java
+++ src/main/java/com/nutriquestion/nutriquestion/config/CorsConfiguration.java
@@ -1,16 +1,27 @@
-//package com.nutriquestion.nutriquestion.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfiguration implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//            .allowedOrigins("https://nutri-quest.netlify.app/")
-//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-//    }
-//}
+package com.nutriquestion.nutriquestion.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+		
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+				.allowedHeaders("*")
+				.allowedOrigins("https://nutri-quest.netlify.app/");
+				
+			}
+		
+		};
+		
+	}
+}
