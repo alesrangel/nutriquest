76ae1bbe86ba38eb886cab1cebf7054105367b56
alesrangel <alesrangel15@gmail.com>
2022-05-24 22:39:28 -0300

    exibi apenas arquivados

--- src/main/java/com/nutriquestion/nutriquestion/repositories/PacienteRepository.java
+++ src/main/java/com/nutriquestion/nutriquestion/repositories/PacienteRepository.java
@@ -12,10 +12,10 @@ import com.nutriquestion.nutriquestion.entities.Questionario;
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
 
-	@Query(nativeQuery = true, value = "SELECT * FROM tb_paciente WHERE tb_paciente.arquivado = true")
+	@Query(nativeQuery = true, value = "SELECT * FROM tb_paciente WHERE tb_paciente.nutricionista_id = :nutricionistaId and tb_paciente.arquivado = true")
 	List<Paciente> findArquivados();
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM tb_paciente WHERE tb_paciente.nutricionista_id = :nutricionistaId")
+	@Query(nativeQuery = true, value = "SELECT * FROM tb_paciente WHERE tb_paciente.nutricionista_id = :nutricionistaId and tb_paciente.arquivado = false")
 	List<Paciente> findByNutricionista(Long nutricionistaId);
 
 	
