7d9cfb706813aa2c4cd8d7e8b99dd29c41bf6555
alesrangel <alesrangel15@gmail.com>
2022-05-25 17:57:35 -0300

    order by respostas

--- src/main/java/com/nutriquestion/nutriquestion/repositories/QuestaoRepository.java
+++ src/main/java/com/nutriquestion/nutriquestion/repositories/QuestaoRepository.java
@@ -13,7 +13,7 @@ import com.nutriquestion.nutriquestion.entities.Questao;
 @Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Long>{
 
-	@Query(nativeQuery = true, value = "select * from tb_questao where tb_questao.questionario_id = :idQuest order by tb_resposta.id")
+	@Query(nativeQuery = true, value = "select * from tb_questao where tb_questao.questionario_id = :idQuest ")
 	List<Questao> findByQestaoQuestionario(@Param("idQuest") Long idQuestionario);
 	
 	@Query(nativeQuery = true, value = "UPDATE tb_questao SET resposta_id = :respostaId  WHERE tb_questao.id = :idQuestao")
